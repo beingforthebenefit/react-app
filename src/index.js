b097886044505512efe87244a594b3b93a5f0317
Gerald Todd <geraldt@scan123.com>
2020-06-25 15:26:57 -0700

    Correct winning state and whitespace.

--- src/index.js
+++ src/index.js
@@ -85,7 +85,7 @@ class Game extends React.Component {
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
-
+  
     return (
       <div className="game">
         <div className="game-board">
@@ -110,7 +110,7 @@ function calculateWinner(squares) {
     [6, 7, 8],
     [0, 3, 6],
     [1, 4, 7],
-    [2, 4, 8],
+    [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
   ];
